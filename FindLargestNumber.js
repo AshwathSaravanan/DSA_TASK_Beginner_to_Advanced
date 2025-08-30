c1bb6ebf427e3161e5db91dbfc62fae01d539d0b
AshwathSaravanan <ashwathstunner7@gmail.com>
2025-08-30 18:46:14 +0530

    Loop2LatestChanges

--- FindLargestNumber.js
+++ FindLargestNumber.js
@@ -1,16 +1,20 @@
 //Write a Function that returns the largest number in an array
 
 function findLargestNumber(arr) {
-    let previousmaxNumber=null;
+    let previousmaxNumber=arr[0];//-Infinity
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > previousmaxNumber) {
+        if (arr[i] > previousmaxNumber) { 
             previousmaxNumber = arr[i];
         } 
     }
     return previousmaxNumber;
-}
+} 
 
-let arr = [5, 0, 7, 10, 8, 17, 1];
+//let arr = [5, 0, 7, 10, 8, 17, 1];
+let arr = [5,-9,-19,-3];
+
+
+
 let result = findLargestNumber(arr);
 
 console.log(result);
